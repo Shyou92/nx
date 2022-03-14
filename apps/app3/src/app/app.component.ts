66353174843e7f06e8f899a3c7c0b56b38cd83ee
Shyou92 <vitalex92@yandex.ru>
2022-03-14 18:05:42 +0300

    app3

---
+++ apps/app3/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'apps-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'app3';
+  image = '../assets/images/anotherCat.jpg';
+  class = 'theme-3';
+}
